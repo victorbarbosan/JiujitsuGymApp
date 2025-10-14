845c38003d255b1b5e18c00beae99fc974d07e1c
Victor Barbosa <105869038+victorbarbosan@users.noreply.github.com>
2025-10-14 17:36:10 -0400

    Create a basic Product list

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using JiujitsuGymApp.Models;
+
+namespace JiujitsuGymApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Sample list of products
+            var products = new List<Product>
+            {
+                new Product {Id =1, Name = "Laptop", Price = 1200.00m},
+                new Product {Id = 2, Name = "Smartphone", Price = 800.00m},
+                new Product {Id =3, Name = "Headphones", Price = 150.00m}
+            };
+            return View(products);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var product = 
+            return View(product);
+        }
+    }
+}
