7561c002c859b99f8ce9b877173febcc81f582e2
Victor Barbosa <105869038+victorbarbosan@users.noreply.github.com>
2025-11-15 10:03:38 -0500

    Add Classes page and navigation link
    Introduces a ClassesController and corresponding Index view to display a list of classes. Updates the main layout to include a navigation link to the new Classes page.

---
+++ Controllers/ClassesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace JiujitsuGymApp.Controllers
+{
+	public class ClassesController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
