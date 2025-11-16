eb2918fee7f3b96ed14448c31f496f3484aaba47
Victor Barbosa <105869038+victorbarbosan@users.noreply.github.com>
2025-11-16 13:30:24 -0500

    Restructure: move repository to solution level

---
+++ JiujitsuGymApp/Models/User.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JiujitsuGymApp.Models
+{
+	public enum UserRole
+	{
+		Admin = 1,
+		Teacher = 2,
+		Student = 3,
+		Staff = 4
+	}
+
+	public class User
+	{
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int Id { get; set; }
+		[Required, StringLength(100)]
+		public string Name { get; set; }
+		[Required, StringLength(50)]
+		public UserRole Role { get; set; }
+		[StringLength(30)]
+		public string? Belt { get; set; } = "White";
+		[Phone]
+		[StringLength(20)]
+		public string? PhoneNumber { get; set; }
+		[EmailAddress, StringLength(100)]
+		public string? Email { get; set; }
+		[StringLength(200)]
+		public string? Address { get; set; }
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+		public DateTime? DeletedAt { get; set; }
+	}
+}
