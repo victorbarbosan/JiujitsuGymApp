0a3316bd18790f5e97158beca712322cf8583881
Victor Barbosa <105869038+victorbarbosan@users.noreply.github.com>
2025-12-10 08:29:55 -0500

    Create LoginViewModel.cs

---
+++ JiujitsuGymApp/Models/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JiujitsuGymApp.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; } = string.Empty;
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+}
